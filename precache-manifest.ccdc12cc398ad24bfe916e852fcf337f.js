ca03fa1a91a41e6c2e532f3f65e8ff912eb53dd2
Digitalplanets <digitalplanetstudios@gmail.com>
2023-01-02 15:04:03 -0500

    Updates

---
+++ precache-manifest.ccdc12cc398ad24bfe916e852fcf337f.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d3f021984dcb2894a25edd38d3ddc86",
+    "url": "/theplanet/index.html"
+  },
+  {
+    "revision": "32a957d3fc4b7c6ddb5c",
+    "url": "/theplanet/static/css/main.99e75adc.chunk.css"
+  },
+  {
+    "revision": "8de18bad10b10748f39b",
+    "url": "/theplanet/static/js/2.71258f5b.chunk.js"
+  },
+  {
+    "revision": "914a85462fc888825cb3a498c2b9869c",
+    "url": "/theplanet/static/js/2.71258f5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32a957d3fc4b7c6ddb5c",
+    "url": "/theplanet/static/js/main.92e7ed72.chunk.js"
+  },
+  {
+    "revision": "b43c9b1f3e58775182cc",
+    "url": "/theplanet/static/js/runtime-main.c2f837af.js"
+  },
+  {
+    "revision": "ab84b033cfd2b64f8b85a2622945f53c",
+    "url": "/theplanet/static/media/new.ab84b033.png"
+  },
+  {
+    "revision": "8de612350ab2a6a38c685959bfaf483f",
+    "url": "/theplanet/static/media/office.8de61235.png"
+  },
+  {
+    "revision": "fd946b64ec1052809970240fd8440889",
+    "url": "/theplanet/static/media/planet1.fd946b64.png"
+  },
+  {
+    "revision": "f1f59afd0946c4f78b8ab814caa18725",
+    "url": "/theplanet/static/media/video.f1f59afd.mp4"
+  },
+  {
+    "revision": "53bd42319f5553be143b1666856ce192",
+    "url": "/theplanet/static/media/welcome.53bd4231.png"
+  },
+  {
+    "revision": "ef556b3a72d403b48bb1705350415afa",
+    "url": "/theplanet/static/media/world2.ef556b3a.png"
+  },
+  {
+    "revision": "aae58f993ee3873e013b8e3069799a39",
+    "url": "/theplanet/static/media/writing.aae58f99.png"
+  }
+]);
