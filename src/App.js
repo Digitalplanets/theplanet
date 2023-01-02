4d66e5526753b31bbae8cae53f1fa4a42807cdc7
Digitalplanets <digitalplanetstudios@gmail.com>
2023-01-02 15:11:06 -0500

    route

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <Router>
       <Switch>
-        <Route path='/' component={Home} exact />
+        <Route path='/theplanet' component={Home} exact />
         <Route path='/signin' component={SigninPage} exact />
       </Switch>
     </Router>
